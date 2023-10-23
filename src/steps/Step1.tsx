6ac72f8ffad0d5d0eb7d2cda0ad10bd9081d7a30
Temi-pinheiro <temitopepinheiro@gmail.com>
2023-10-23 22:35:17 +0100

    add bounce to github footer

--- src/steps/Step1.tsx
+++ src/steps/Step1.tsx
@@ -1,4 +1,4 @@
-import { Header } from '../components';
+import { Header, TextInput } from '../components';
 
 export const Step1 = ({ data, update }: { data: any; update: any }) => {
   console.log({ data, update });
@@ -8,6 +8,14 @@ export const Step1 = ({ data, update }: { data: any; update: any }) => {
         title='Personal info'
         details='Please provide your name, email address, and phone number.'
       />
+      <div className='flex flex-col gap-y-6 md:mt-[35px]'>
+        <TextInput
+          label='Name'
+          name='name'
+          value={data.name}
+          handleInputChange={update}
+        />
+      </div>
     </div>
   );
 };
